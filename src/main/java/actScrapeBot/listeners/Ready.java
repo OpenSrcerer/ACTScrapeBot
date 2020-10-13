0324edbe39a3cd420fff457303321f525d0640f3
OpenSrcerer <weedpot@fhf.edu.al>
2020-10-13 23:42:37 +0300

    Added proper listeners in Ready.java

--- src/main/java/actScrapeBot/listeners/Ready.java
+++ src/main/java/actScrapeBot/listeners/Ready.java
@@ -1,5 +1,11 @@
 package actScrapeBot.listeners;
 
+import actScrapeBot.ScrapeBot;
+import actScrapeBot.commands.getCourses.CoursesReceiver;
+import actScrapeBot.commands.getResources.ResourcesReceiver;
+import actScrapeBot.commands.login.LoginReceiver;
+import actScrapeBot.commands.logout.LogoutReceiver;
+import actScrapeBot.managers.RequestManager;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -8,6 +14,14 @@ public class Ready extends ListenerAdapter {
 
     @Override
     public void onReady(@NotNull ReadyEvent event) {
-
+        // Triggers command initiation.
+        ScrapeBot.ScrapeBot.addEventListener(new CommandTrigger());
+        // Triggers request processing
+        RequestManager.getInstance().addListener(
+                new LoginReceiver(),
+                new LogoutReceiver(),
+                new ResourcesReceiver(),
+                new CoursesReceiver()
+        );
     }
 }
