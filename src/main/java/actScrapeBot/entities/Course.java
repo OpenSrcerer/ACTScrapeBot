f51fa221c69303e4fb2bfd5022f9720bbaa28570
OpenSrcerer <weedpot@fhf.edu.al>
2020-10-13 23:37:38 +0300

    Updated Course.java with a proper POJO implementation of Course object management.

--- src/main/java/actScrapeBot/entities/Course.java
+++ src/main/java/actScrapeBot/entities/Course.java
@@ -1,8 +1,58 @@
 package actScrapeBot.entities;
 
+import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+
 public class Course {
 
-    public Course() {
+    private final String courseName, courseLecturer, courseTerm;
+    // The variable below holds the information
+    // within the course. Anything
+    // posted by the lecturer goes here.
+    private final Map<String, List<String>> resources;
 
+    public Course(String cName, String cLecturer, String cTerm, Map<String, List<String>> res) throws IllegalArgumentException {
+        if (cName == null) {
+            throw new IllegalArgumentException("Provided Course Name was null!");
+        } else if (cLecturer == null) {
+            throw new IllegalArgumentException("Provided Course Lecturer was null!");
+        } else if (cTerm == null) {
+            throw new IllegalArgumentException("Provided Course Term was null!");
+        } else if (res == null) {
+            throw new IllegalArgumentException("Provided Course Resources were null!");
+        }
+
+        courseName = cName;
+        courseLecturer = cLecturer;
+        courseTerm = cTerm;
+        resources = res;
+    }
+
+    @Nullable
+    public List<String> getResources(String key) {
+        return resources.get(key);
+    }
+
+    @Nonnull
+    public Map<String, List<String>> getResources() {
+        return resources;
+    }
+
+    @Nonnull
+    public String getCourseLecturer() {
+        return courseLecturer;
+    }
+
+    @Nonnull
+    public String getCourseName() {
+        return courseName;
+    }
+
+    @Nonnull
+    public String getCourseTerm() {
+        return courseTerm;
     }
 }
