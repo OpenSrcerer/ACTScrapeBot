5c60a21135b19593c21e64f8b7f30345c259c20c
OpenSrcerer <weedpot@fhf.edu.al>
2020-10-14 22:33:15 +0300

    Added password member in ActUser

--- src/main/java/actScrapeBot/entities/ActUser.java
+++ src/main/java/actScrapeBot/entities/ActUser.java
@@ -1,16 +1,30 @@
 package actScrapeBot.entities;
 
+/**
+ * Manages instances of an ACT User.
+ */
 public class ActUser {
-    private final String discordUserId, actUsername;
+    private final String discordUserId, actUsername, actPassword;
 
-    public ActUser(String dId) {
-        discordUserId = dId;
-        actUsername = null;
+    public ActUser() {
+        discordUserId = "";
+        actUsername = "";
+        actPassword = "";
     }
 
-    public ActUser(String dId, String actUser) {
-        actUsername = actUser;
+    public ActUser(String dId, String actUser, String actPass) throws IllegalArgumentException {
+
+        if (dId == null) {
+            throw new IllegalArgumentException("Provided Discord ID was null!");
+        } else if (actUser == null) {
+            throw new IllegalArgumentException("Provided Act Username was null!");
+        } else if (actPass == null) {
+            throw new IllegalArgumentException("Provided Act Password was null!");
+        }
+
         discordUserId = dId;
+        actUsername = actUser;
+        actPassword = actPass;
     }
 
     public String getDiscordUserId() {
@@ -19,5 +33,9 @@ public class ActUser {
 
     public String getActUsername() {
         return actUsername;
+    }
+
+    public boolean isAnonymous() {
+        return actUsername.isEmpty() || discordUserId.isEmpty();
     }
 }
